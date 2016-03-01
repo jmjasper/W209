0e7d543e7ce3ffa609160357e821277421db9aca
James Jasper <jjasper1776@gmail.com>
2016-02-29 20:47:28 -0800

    Started assignment 6

---
+++ assignment_6/data/gen_data.py
@@ -0,0 +1,21 @@
+from __future__ import division
+import random
+import csv
+
+index = []
+results = []
+j = 0
+
+for i in range(0,1000):
+	if random.randint(0,1) == 1:
+		j = j + 1
+	#print j / (i + 1)
+	r = j / (i + 1)
+	index.append(i + 1)
+	results.append(r)
+
+with open('data.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(["Index","Result"])
+    for i in range(0,len(index)):
+    	writer.writerow([index[i], results[i]])
